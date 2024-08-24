ab84f872f5e557686dfabbf761f1badf46acd3a8
sixthson6 <sudais2408@gmail.com>
2024-08-24 04:31:11 +0000

    Add shop page components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.components'
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
